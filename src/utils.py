0d1ead14a5f19f74a220db0775af0dc56f4b8c4b
Rafe2001 <khanabdulrafe48@gmail.com>
2023-10-26 20:58:44 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV  
 from sklearn.model_selection import train_test_split
 
 def save_object(file_path, obj):
@@ -19,13 +20,19 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)  
     
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         
-        for i in range(len(list(models))):
+        for i in range(len(list(models))):  
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            rs = RandomizedSearchCV(model, para, cv=3)
+            rs.fit(X_train, y_train)
+            
+            model.set_params(**rs.best_params_)
             model.fit(X_train, y_train)
+            #model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train_pred, y_train)
